4054f0b001b91124c67f46a3cfc04f695df3fe9f
John <john@r73.de>
2022-05-31 21:15:09 +0200

    initial commit using Spyder

---
+++ draft.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 31 20:58:01 2022
+
+@author: john
+"""
+from room import Raum
+
+
+print("hallo!")
+r = Raum(hoehe=12, bezeichnung="PetersRaum", raumnummer=200)
+r.printMe()
