42b0ae79a1fcc2368d86cac438aacd43b81e5a61
John <john@r73.de>
2022-06-20 21:02:11 +0200

    added classes

---
+++ gruppenarbeit/wall.py
@@ -0,0 +1,12 @@
+class Wand:
+    height = 0.0
+    name = ""
+    number = 0
+
+    def __init__(self, hoehe, bezeichnung, raumnummer):
+        self.height = hoehe
+        self.name = bezeichnung
+        self.number = raumnummer
+
+    def print_me(self):
+        return f"Raum {self.number}: Bezeichnung: {self.name}, Höhe (in m): {self.height}"
---
+++ gruppenarbeit/window.py
@@ -0,0 +1,16 @@
+from opening import Oeffnung
+
+
+class Fenster(Oeffnung):
+
+    height = 0.0
+    name = ""
+    number = 0
+
+    def __init__(self, hoehe, bezeichnung, raumnummer):
+        self.height = hoehe
+        self.name = bezeichnung
+        self.number = raumnummer
+
+    def print_me(self):
+        return f"Raum {self.number}: Bezeichnung: {self.name}, Höhe (in m): {self.height}"
