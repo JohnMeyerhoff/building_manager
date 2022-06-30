85130abe8f0eef4324cfc43411e030ff3d932535
John <john@r73.de>
2022-06-30 21:35:58 +0200

    add opening method area

--- gruppenarbeit/opening.py
+++ gruppenarbeit/opening.py
@@ -1,6 +1,6 @@
 class Oeffnung:
     height = 0.0
-    width = ""
+    width = 0.0
     diameter = 0
 
     def __init__(self, hoehe, breite, durchmesser):
@@ -10,3 +10,18 @@ class Oeffnung:
 
     def print_me(self):
         return f"Öffnung: Breite: {self.width}, Durchmesser: {self.diameter}, Höhe (in m): {self.height}"
+
+    def vob_sub_area(self):
+        if self.area() < 2.500:
+            return 0.0
+        else:
+            return self.area()
+
+    def area(self):
+        """
+         wird verwendet, um später zu prüfen, ob nach der
+        Abrechnungsvorschrift der VOB die Fläche der Öffnung von der
+        Fläche der Wand abgezogen werden darf.
+        :return: fläche der Öffnung
+        """
+        return self.width * self.height
